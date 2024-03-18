a50b4fd8ddee4db8ae4de18196bfd6884c31dea1
SOHAG-SHU <sohag209366@gmail.com>
2024-03-18 23:25:59 +0600

    Problem E4.23 part-2

--- src/BalloonAndBalloonTester.java
+++ src/BalloonAndBalloonTester.java
@@ -5,7 +5,6 @@ public class BalloonAndBalloonTester {
         System.out.println("Volume of the balloon after adding 100 cm^3 of air: " + myBalloon.getVolume() + " cm^3");
         System.out.println("Surface area of the balloon after adding 100 cm^3 of air: " + myBalloon.getSurfaceArea() + " cm^2");
         System.out.println("Radius of the balloon after adding 100 cm^3 of air: " + myBalloon.getRadius() + " cm");
-
         myBalloon.addAir(100);
         System.out.println("\nVolume of the balloon after adding another 100 cm^3 of air: " + myBalloon.getVolume() + " cm^3");
         System.out.println("Surface area of the balloon after adding another 100 cm^3 of air: " + myBalloon.getSurfaceArea() + " cm^2");
